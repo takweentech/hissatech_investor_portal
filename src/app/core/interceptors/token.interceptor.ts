d25557f0930555a7ee7807159d0ad7ec3a95d7e8
zatattou <z.atattou@takweentech.sa>
2025-08-14 18:30:22 +0100

    remove hardcoded token

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -9,8 +9,8 @@ export const authInterceptor = (req: HttpRequest<unknown>, next: HttpHandlerFn) 
     const token = tokenService.getToken();
     if (token) {
         const cloned = req.clone({
-            // headers: req.headers.set('Authorization', `Bearer ${token}`)
-            headers: req.headers.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5ZWU3MmJjNi0yYTFjLTQ5YTgtYjUzMy0wNTUzNmEwNTNlNjIiLCJzdWIiOiI1MTIzNDU2NzciLCJVc2VySWQiOiI5MzlhZDEwMC1iZGQ0LTQxMDktODcwZi1kNWM1OTZhNDUzZDciLCJFbWFpbCI6InkuYWxhYUB0YWt3ZWVudGVjaC5zYSIsIlJvbGUiOiJJbnZlc3RvciIsIk1vYmlsZU51bWJlciI6IjUxMjM0NTY3NyIsImV4cCI6MTc2MjM0NTQwNCwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwMSIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDEifQ.pnXY63zIDSHCPSqtDe5IeNB5Cr20uOhIuED3kvvUInk`)
+            headers: req.headers.set('Authorization', `Bearer ${token}`)
+            // headers: req.headers.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5ZWU3MmJjNi0yYTFjLTQ5YTgtYjUzMy0wNTUzNmEwNTNlNjIiLCJzdWIiOiI1MTIzNDU2NzciLCJVc2VySWQiOiI5MzlhZDEwMC1iZGQ0LTQxMDktODcwZi1kNWM1OTZhNDUzZDciLCJFbWFpbCI6InkuYWxhYUB0YWt3ZWVudGVjaC5zYSIsIlJvbGUiOiJJbnZlc3RvciIsIk1vYmlsZU51bWJlciI6IjUxMjM0NTY3NyIsImV4cCI6MTc2MjM0NTQwNCwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwMSIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDEifQ.pnXY63zIDSHCPSqtDe5IeNB5Cr20uOhIuED3kvvUInk`)
         });
         return next(cloned);
     }
